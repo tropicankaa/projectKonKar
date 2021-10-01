7a0dbd4bb67261faa145ac9204741a6c1dbe8b4f
session1 <session1>
2021-10-01 09:44:51 +0700

    Initial Commit

---
+++ projectKonKar/projectKonKarApp.swift
@@ -0,0 +1,17 @@
+//
+//  projectKonKarApp.swift
+//  projectKonKar
+//
+//  Created by user on 01.10.2021.
+//
+
+import SwiftUI
+
+@main
+struct projectKonKarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
